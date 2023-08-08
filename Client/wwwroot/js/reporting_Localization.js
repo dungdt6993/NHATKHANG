4cdbb731441bd8b82cde84701c5dddb85c963fbd
DINH TIEN DUNG <DINH TIEN DUNG@DESKTOP-LHKLGN1>
2023-08-08 18:04:46 +0700

    update

--- Client/wwwroot/js/reporting_Localization.js
+++ Client/wwwroot/js/reporting_Localization.js
@@ -1,12 +1,12 @@
 ﻿window.ReportingLocalization = {
     onCustomizeLocalization: function(s, e) {
-        $.get("/js/localization/de.json").done(result => {
+        $.get("/js/localization/vi.json").done(result => {
             e.WidgetLocalization.loadMessages(result);
         }).always(() => {
-            e.WidgetLocalization.locale("de");
+            e.WidgetLocalization.locale("vi");
         })
 
-        e.LoadMessages($.get("/js/localization/dx-analytics-core.de.json"));
-        e.LoadMessages($.get("/js/localization/dx-reporting.de.json"));
+        e.LoadMessages($.get("/js/localization/dx-analytics-core.vi.json"));
+        e.LoadMessages($.get("/js/localization/dx-reporting.vi.json"));
     }
 }
